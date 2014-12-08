d6156d096abb10d70573a9d0ee9bcc679a6bb21b
Derek Fermaint <derek.j.fermaint@gmail.com>
2014-12-08 15:00:56 -0700

    Commit

---
+++ Carpet Price Calculator/Carpet Price Calculator.Designer.vb
@@ -0,0 +1,261 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtCost = New System.Windows.Forms.TextBox()
+        Me.txtStyle = New System.Windows.Forms.TextBox()
+        Me.txtColor = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtArea = New System.Windows.Forms.TextBox()
+        Me.txtLength = New System.Windows.Forms.TextBox()
+        Me.txtWidth = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtTotCost = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtCost)
+        Me.GroupBox1.Controls.Add(Me.txtStyle)
+        Me.GroupBox1.Controls.Add(Me.txtColor)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(9, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(172, 102)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Carpet Data"
+        '
+        'txtCost
+        '
+        Me.txtCost.Location = New System.Drawing.Point(64, 76)
+        Me.txtCost.Name = "txtCost"
+        Me.txtCost.Size = New System.Drawing.Size(100, 20)
+        Me.txtCost.TabIndex = 9
+        '
+        'txtStyle
+        '
+        Me.txtStyle.Location = New System.Drawing.Point(62, 50)
+        Me.txtStyle.Name = "txtStyle"
+        Me.txtStyle.Size = New System.Drawing.Size(100, 20)
+        Me.txtStyle.TabIndex = 8
+        '
+        'txtColor
+        '
+        Me.txtColor.Location = New System.Drawing.Point(63, 24)
+        Me.txtColor.Name = "txtColor"
+        Me.txtColor.Size = New System.Drawing.Size(100, 20)
+        Me.txtColor.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(9, 67)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 26)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Cost per " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "    Sq Ft"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 48)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Style:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 27)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Color:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtArea)
+        Me.GroupBox2.Controls.Add(Me.txtLength)
+        Me.GroupBox2.Controls.Add(Me.txtWidth)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Location = New System.Drawing.Point(187, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(183, 102)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Room Size"
+        '
+        'txtArea
+        '
+        Me.txtArea.Enabled = False
+        Me.txtArea.Location = New System.Drawing.Point(83, 76)
+        Me.txtArea.Name = "txtArea"
+        Me.txtArea.Size = New System.Drawing.Size(100, 20)
+        Me.txtArea.TabIndex = 12
+        '
+        'txtLength
+        '
+        Me.txtLength.Location = New System.Drawing.Point(83, 50)
+        Me.txtLength.Name = "txtLength"
+        Me.txtLength.Size = New System.Drawing.Size(100, 20)
+        Me.txtLength.TabIndex = 11
+        '
+        'txtWidth
+        '
+        Me.txtWidth.Location = New System.Drawing.Point(83, 27)
+        Me.txtWidth.Name = "txtWidth"
+        Me.txtWidth.Size = New System.Drawing.Size(100, 20)
+        Me.txtWidth.TabIndex = 10
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(45, 79)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(32, 13)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Area:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(34, 53)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(43, 13)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Length:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(39, 30)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(38, 13)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Width:"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(13, 150)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(115, 30)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(134, 150)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(115, 30)
+        Me.btnClear.TabIndex = 3
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(255, 150)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(115, 30)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(85, 123)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Total Cost:"
+        '
+        'txtTotCost
+        '
+        Me.txtTotCost.Enabled = False
+        Me.txtTotCost.Location = New System.Drawing.Point(149, 120)
+        Me.txtTotCost.Name = "txtTotCost"
+        Me.txtTotCost.Size = New System.Drawing.Size(100, 20)
+        Me.txtTotCost.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(383, 203)
+        Me.Controls.Add(Me.txtTotCost)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Carpet Price Calculator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtTotCost As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtCost As System.Windows.Forms.TextBox
+    Friend WithEvents txtStyle As System.Windows.Forms.TextBox
+    Friend WithEvents txtColor As System.Windows.Forms.TextBox
+    Friend WithEvents txtArea As System.Windows.Forms.TextBox
+    Friend WithEvents txtLength As System.Windows.Forms.TextBox
+    Friend WithEvents txtWidth As System.Windows.Forms.TextBox
+
+End Class
---
+++ Carpet Price Calculator/Carpet Price Calculator.vb
@@ -0,0 +1,159 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+
+    '   Gets data from text boxes and stores into rectangle properties
+    Public Sub GetRectData(ByVal objRectangle As Rectangle)
+        '   Assigns values
+        Try
+            objRectangle.Width() = txtWidth.Text()
+            objRectangle.Length() = txtLength.Text()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    '   Gets data from text boxes and stores into carpet properies
+    Public Sub GetCarpetData(ByVal objCarpet As Carpet)
+        '   Assigns values
+        Try
+            objCarpet.Color() = txtColor.Text()
+            objCarpet.Style() = txtStyle.Text()
+            objCarpet.Cost() = txtCost.Text()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    '   Calculates the total cost
+    Public Sub TotalCost(ByVal objCarpet As Carpet, ByVal objRectangle As Rectangle)
+        Dim totalCost As Double
+        totalCost = objCarpet.Cost() * objRectangle.Area()
+        txtTotCost.Text() = FormatCurrency(totalCost, , , TriState.True, TriState.True)
+    End Sub
+    '   Event to calculate and store data in properties, then display
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim rect As New Rectangle
+        Dim carp As New Carpet
+
+        '   Rectangle start up
+        GetRectData(rect)
+        txtArea.Text() = rect.Area()
+
+        '   Carpet start up
+        GetCarpetData(carp)
+
+        '   Displays total cost
+        TotalCost(carp, rect)
+    End Sub
+
+    '   Clears data
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtColor.Clear()
+        txtStyle.Clear()
+        txtCost.Clear()
+        txtLength.Clear()
+        txtWidth.Clear()
+        txtArea.Clear()
+        txtTotCost.Clear()
+    End Sub
+
+    '   Closes form
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Hide()
+    End Sub
+End Class
+
+Public Class Rectangle
+    '   Member variables
+    Dim sinWidth As Single
+    Dim sinLength As Single
+    Dim sinArea As Single
+
+    '   Constructor
+    Public Sub New()
+        Debug.WriteLine("Rectangle object being created.")
+        sinWidth = 0
+        sinLength = 0
+        sinArea = 0
+    End Sub
+
+    '   Width property procedure
+    Public Property Width() As Single
+        Get
+            Return sinWidth
+        End Get
+        Set(value As Single)
+            sinWidth = value
+        End Set
+    End Property
+
+    '   Height property procedure
+    Public Property Length() As Single
+        Get
+            Return sinLength
+        End Get
+        Set(value As Single)
+            sinLength = value
+        End Set
+    End Property
+
+    '   Calculates the area
+    Public Sub CalcArea()
+        sinArea = sinLength * sinWidth
+    End Sub
+
+    '   Area property procedure
+    Public ReadOnly Property Area() As Single
+        Get
+            CalcArea()
+            Return sinArea
+        End Get
+    End Property
+End Class
+
+Public Class Carpet
+    '   Member variables
+    Dim strColor As String
+    Dim strStyle As String
+    Dim dblCost As Decimal
+
+    '   Constructor
+    Public Sub New()
+        Debug.WriteLine("Carpet object is being created.")
+        strColor = ("unknown")
+        strStyle = ("unknown")
+        dblCost = 0.0
+    End Sub
+
+    '   Color property procedure
+    Public Property Color() As String
+        Get
+            Return strColor
+        End Get
+        Set(value As String)
+            strColor = value
+        End Set
+    End Property
+
+    '   Style property procedure
+    Public Property Style() As String
+        Get
+            Return strStyle
+        End Get
+        Set(value As String)
+            strStyle = value
+        End Set
+    End Property
+
+    '   Cost property procedure
+    Public Property Cost() As Decimal
+        Get
+            Return dblCost
+        End Get
+        Set(value As Decimal)
+            dblCost = value
+        End Set
+    End Property
+End Class
